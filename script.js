875bd9f8929438ec2ad32cc29cb232d00f746157
BidibulleKing <70601660+BidibulleKing@users.noreply.github.com>
2022-11-20 20:59:53 +0100

    Add comments

--- script.js
+++ script.js
@@ -4,21 +4,27 @@ const app = {
   result: document.querySelector("#result"),
 
   convert: function (sentence) {
-    // Je dois aller au magasin. => Aller au magasin, je dois.
+    // TODO : Je dois aller au magasin. => Aller au magasin, je dois.
+    // Filtrer les ponctuations
     let filteredSentence = sentence
       .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "")
       .toLowerCase();
+    // Spliter la string en array, puis filtrer les espaces excédents
     const words = filteredSentence.split(" ").filter((e) => e !== "");
     const wordsNumber = words.length;
+    // Récupérer les deux derniers mots, et y ajouter une virgule
     let newSentence = [words[wordsNumber - 2], words[wordsNumber - 1] + ","];
     const result = this.result;
 
+    // Ajouter les mots restants, à la suite des deux derniers mots
     for (let i = 0; i < wordsNumber - 2; i++) {
       newSentence.push(words[i]);
     }
 
+    // Array to string
     newSentence = newSentence.join(" ");
 
+    // Affichage + ajout de la ponctuation "."
     result.textContent =
       newSentence.charAt(0).toUpperCase() + newSentence.slice(1) + ".";
 
